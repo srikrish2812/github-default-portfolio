4811ea853afae072c1151663ad0e8c719c615783
Abhay Kumara Sri Krishna Nandiraju <98251146+srikrish2812@users.noreply.github.com>
2024-09-16 01:17:24 +0000

    changed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Abhay Nandiraju",
+  title: "AI/ML Engineer",
+  email: "abhay2001sri@gmail.com",
+  gitHub: "https://github.com/srikrish2812",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
